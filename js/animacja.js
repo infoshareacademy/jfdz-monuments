b7f0eceb6bbc8f029cd402fc0bf3ab380ac04d19
konrad5 <konradkc@op.pl>
2016-02-06 16:50:06 +0100

    update marginesy i paddingi

---
+++ js/animacja.js
@@ -0,0 +1,16 @@
+/**
+ * Created by konrad5 on 06.02.16.
+ */
+
+
+function wplywanieElementow(){
+$('.zajawka-tekst').
+
+};
+
+
+//wywolanie funkcji przy uruchomieniu strony:
+
+$(document).ready(function () {
+    wplywanieElementow();
+});
