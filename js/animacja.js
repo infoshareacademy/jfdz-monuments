b6866f81aa419f3786f2c1a23241188923a42472
konrad5 <konradkc@op.pl>
2016-02-07 18:35:57 +0100

    Merge branch 'develop' of https://github.com/infoshareacademy/jfdz-monuments into animacja
    Conflicts:
    	index.html

---
+++ js/animacja.js
@@ -0,0 +1,15 @@
+/**
+ * Created by konrad5 on 06.02.16.
+ */
+
+
+function wplywanieElementow(){
+//$('.zajawka-tekst').fadeIn(1000);
+};
+
+
+//wywolanie funkcji przy uruchomieniu strony:
+
+$(document).ready(function () {
+    wplywanieElementow();
+});
