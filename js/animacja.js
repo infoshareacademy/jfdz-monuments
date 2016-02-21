c3c2815d131b48c4afb04fca9fff9c84b5a39544
darekcz <niechcemaila@gmail.com>
2016-02-21 18:39:51 +0100

    sticky menu scroll

--- js/animacja.js
+++ js/animacja.js
@@ -61,3 +61,28 @@ $(document).on('scroll', onScroll);
             });
         });
     });
+
+//na przyszlosc stopka fade in - darek
+//$(document).ready(function() {
+//
+//    /* Every time the window is scrolled ... */
+//    $(window).scroll( function(){
+//
+//        /* Check the location of each desired element */
+//        $('.hideme').each( function(i){
+//
+//            var bottom_of_object = $(this).offset().top + // $(this).outerHeight();
+//            var bottom_of_window = $(window).scrollTop() + $(window).height();
+//
+//            /* If the object is completely visible in the window, fade it it */
+//            if( bottom_of_window > bottom_of_object ){
+//
+//                $(this).animate({'opacity':'1'},500);
+//
+//            }
+//
+//        });
+//
+//    });
+//
+//});
