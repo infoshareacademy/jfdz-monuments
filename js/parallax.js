e4b945a22b3561888603ca28afe7e6bdf8157421
marcinplocki <plockione@gmail.com>
2016-02-22 13:19:55 +0100

    regulacja css

--- js/parallax.js
+++ js/parallax.js
@@ -2,5 +2,5 @@
 $(window).on('scroll load',function (e) {
     var scrolled = $(window).scrollTop();
     $('.zajawka').css('background-position', 'center ' + -(-122 +scrolled * 0.5) + 'px');
-    $('.zajawka-tekst').css('margin-top', scrolled * 0.6);
+    $('.zajawka-tekst').css('margin-top', scrolled * 0.75);
 });
