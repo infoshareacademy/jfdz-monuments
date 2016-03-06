e3db595f2732d6dc7adeb424c6307fa230bf77d9
darekcz <niechcemaila@gmail.com>
2016-03-06 17:21:43 +0100

    gra - tabelka

---
+++ js/gra.js
@@ -0,0 +1,32 @@
+//var $container = $('<div id="app">');
+//
+//var $table = $('<table>');
+//var size = 8;
+//
+//
+//
+//for (var i=0; i<size; i++) {
+//    var $row = $('<tr>');
+//    for (var j=0;j<size;j++) {
+//        var $cell = $('<td>');
+//        $cell.css({
+//            border: '1px solid black',
+//            width: 50,
+//            height: 50,
+//            backgroundColor: 'yellow'
+//        });
+//        $row.append($cell);
+//    }
+//    $table.append($row);
+//}
+//$container.append($table);
+//$('body').append($container);
+
+
+var $
+
+
+
+
+
+
