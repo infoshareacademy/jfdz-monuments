f16731164f8718c48202eac492d47a66ee72e95a
Halina <hmd@wp.pl>
2016-03-07 18:13:11 +0100

    cookies

---
+++ scripts/cookies.js
@@ -0,0 +1,8 @@
+CookieAlert.init({
+    style: 'dark',
+    position: 'bottom',
+    opacity: '0.9',
+    displayTime: 10000,
+    cookiePolicy: 'http://cookiealert.sruu.pl/o-ciasteczkach',
+    text: 'Ten serwis wykorzystuje pliki cookies. Korzystanie z witryny oznacza zgodę na ich zapis lub odczyt wg ustawień przeglądarki.'
+});
