9b708fc866815d381125cd30aba6d28b6f441091
Konrad5 <konradkc@op.pl>
2016-03-10 22:30:35 +0100

    nawigacja początki, robie sobię plansze

--- js/nawigacja_gry.js
+++ js/nawigacja_gry.js
@@ -1,19 +1,36 @@
 /**
  * Created by Konrad on 2016-03-10.
  */
+var $game = $('#gra');
 var $table = $('<table>');
 var board = [];
 var $row = $('<tr>');
-var $cell =$('<td>');
-var size = 10;
+var $cell = $('<td>');
 
 
-function CreateBoard () {
-    var size =10;
-    for (x=0;x < size; x++) {
-        board[x] =[];
-        console.log('twoja stara');
+$($game).css({
+    backgroundColor: 'grey',
+});
+
+function CreateBoard(height, width) {
+    var $table = $('<table>');
+    for (y = 0; y < height; y++) {
+        $row = $('<tr>');
+        for (x = 0; x < width; x++) {
+            $cell = $('<td>');
+            $cell.css({
+                width: 50,
+                height: 50,
+                border: '1px solid',
+                backgroundColor: 'green'
+            })
+            $cell.html('działa!')
+            $row.append($cell);
+        }
+
+        $table.append($row);
     }
+    return $table;
 }
 
-CreateBoard();
+$game.append(CreateBoard(8, 16));
