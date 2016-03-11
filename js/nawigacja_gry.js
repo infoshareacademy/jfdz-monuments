65532517ff39283a7601cbb22093d57381d4fee5
Konrad5 <konradkc@op.pl>
2016-03-11 02:36:21 +0100

    reagowanie komorek na click

--- js/nawigacja_gry.js
+++ js/nawigacja_gry.js
@@ -2,29 +2,24 @@
  * Created by Konrad on 2016-03-10.
  */
 var $game = $('#gra');
-var $table = $('<table>');
-var board = [];
-var $row = $('<tr>');
-var $cell = $('<td>');
+var $table;
+var $row;
+var $cell;
 
 
-$($game).css({
-    backgroundColor: 'grey',
-});
-
-function CreateBoard(height, width) {
-    var $table = $('<table>');
+//tworzenie planszy do gry:
+function CreateTable(height, width) {
+   $table = $('<table>');
     for (y = 0; y < height; y++) {
-        $row = $('<tr>');
+       $row = $('<tr>');
         for (x = 0; x < width; x++) {
             $cell = $('<td>');
             $cell.css({
                 width: 50,
                 height: 50,
-                border: '1px solid',
-                backgroundColor: 'green'
-            })
-            $cell.html('działa!')
+                border: '1px solid'
+            });
+            $cell.html('działa!');
             $row.append($cell);
         }
 
@@ -33,4 +28,17 @@ function CreateBoard(height, width) {
     return $table;
 }
 
-$game.append(CreateBoard(8, 16));
+$game.append(CreateTable(8, 16));
+
+//interaktywność komórek w tabeli
+//pobierz tabele i wykonaj na niej event click dla komorki:
+$table.on('click', 'td', function () {
+    $(this).addClass('pole');
+});
+
+//ustawienie dla ruchów klawiatury in progress spr trigger i eq
+$table.on('keydown', 'td', function () {
+    if (e.keyCode == 37) {
+    $(this).toggleClass('pole');
+    }
+});
