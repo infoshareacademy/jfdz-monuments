8a0dfdc4eef321d7020a3d2b8ff551afa7c07a33
Konrad5 <konradkc@op.pl>
2016-03-15 20:44:15 +0100

    ruch w prawo przyciskiem klawiatury

--- js/nawigacja_gry.js
+++ js/nawigacja_gry.js
@@ -1,25 +1,26 @@
 /**
  * Created by Konrad on 2016-03-10.
  */
-var $game = $('#gra');
+var $game = $('#game');
 var $table;
 var $row;
 var $cell;
-
+var $playerScore;
 
 //1. tworzenie planszy do gry z zaznaczeiem komorki startowej:
 function CreateTable(height, width) {
-   $table = $('<table>');
+    $table = $('<table>');
     for (y = 0; y < height; y++) {
-       $row = $('<tr>');
+        $row = $('<tr>');
         for (x = 0; x < width; x++) {
             $cell = $('<td>');
-            if (y==0 && x==0) {
+            if (y == 0 && x == 0) {
                 $cell.addClass('czerwony');
                 $cell.html('Ruszaj!');
-            };
-            $row.append($cell);
             }
+            ;
+            $row.append($cell);
+        }
 
         $table.append($row);
     }
@@ -28,40 +29,56 @@ function CreateTable(height, width) {
 
 $game.append(CreateTable(8, 8));
 
-//2. interaktywność komórek w tabeli dla click
-//pobierz tabele i wykonaj na niej event click dla komorki:
-//$table.on('click', 'td', function () {
-//    $(this).addClass('pole');
-//});
+//2. interaktywność komórek w tabeli na click
+var firstCell = $('td').first();
 
-//3. interaktywność dla dla ruchów klawiatury in progress spr trigger i eq
+function makeInteractive() {
+    $table.on('click', 'td', function () {
+        $(this).addClass('czerwony');
+        firstCell.addClass('czerwony');
+        $playerScore = $('#playerScore');
+        $playerScore.html("+1");
+        //wywolaj funkcje ktora doda 1km do tablicy playerScore
+        //wywolaj funkcje ktora odejmie 1km od tablicy distance
+    });}
+makeInteractive();
 
-$(document).on('keydown', function (e)  {
-    if (e.keyCode ==37 || e.which ==38 || e.which ==39 || e.which ==40)
-    {
-        $('tr :nth-child(2) ').addClass('czerwony');
+//3. interaktywność dla ruchów klawiatury
+$(document).on('keydown', function (e) {
+    if (e.keyCode == 39) {
+    firstCell = firstCell.next();
+    firstCell.addClass('czerwony');
+
     }
+    //if (e.keyCode == 40) {
+    //    firstCell.next('tr').addClass('czerwony');
+    //}
 });
 
 
+//$(document).on('keydown', function (e)  {
+//    if (e.keyCode ==37 || e.which ==38 || e.which ==39 || e.which ==40)
+//    {
+//        $('tr :nth-child(2) ').addClass('czerwony');
 //
-//var board = [];
-//function poczatekGry() {
-//    var size = 8;
-//    for (var x = 0; x < size; x++) {
-//        board[x] = [];
-//        for (var y = 0; y < size; y++) {
-//            board[x][y] = {};
-//            board[2][3].addClass('czerwony');
 //        }
 //    }
-//}
-//poczatekGry();
+//);
 
 
+//4. ustawienianie początkowego stanu gry
 
+var state = {
+    scoreName: 'Pokonana odległość',
+    time: 20,
+    score: {
+        player: 0,
+    }
+};
 
-
+$('#startGameButton').click(function () {
+    startGame(state);
+});
 
 
 //. wprowadzenie stanu gry, czyli if ruch o 1 pozycje = zabierz mu 1km z puli i dopisz to sumy przebytych km
