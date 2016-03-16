767dd9d6eb70a5eb915c91b8213d3b6c2354b95c
marcinplocki <plockione@gmail.com>
2016-03-17 00:36:32 +0100

    działa losowanie, jeszcze blokada na powtórzenia i czyszczenie.

--- js/random.js
+++ js/random.js
@@ -7,7 +7,11 @@ function rand(min, max) {
         throw new Error('Funkcja wymaga podania obu argumentów min i max');
     }
     return Math.floor(Math.random() * (max - min + 1)) + min;
+
 }
-
-
-console.log(rand(1, 64));
+function choise() {
+    for (var i = 0; i < 5; i++) {
+        $('table, td').eq(rand(1, 64)).addClass('selected');
+    }
+}
+//console.log(rand(1, 64));
