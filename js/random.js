8438f4bd76ad6cbc9f1bf32663f5cc4aa99605cf
mplocki <plockione@gmail.com>
2016-03-19 17:55:32 +0100

    random

--- js/random.js
+++ js/random.js
@@ -1,3 +1,4 @@
+var imgPoints = [0, 10, 20, 50, 40, 20 ];
 function rand(min, max) {
     var argc = arguments.length;
     if (argc === 1) {
@@ -10,13 +11,32 @@ function rand(min, max) {
 function choise() {
     for (var i = 0; i < 5; i++) {
         var icon = $('table, td').eq(rand(1, 64));
-        $('table td').eq(rand(1, 64)).append(targetImage_1);
-        $('table td').eq(rand(1, 64)).append(targetImage_2);
-        $('table td').eq(rand(1, 64)).append(targetImage_3);
-        $('table td').eq(rand(1, 64)).append(targetImage_4);
-        $('table td').eq(rand(1, 64)).append(targetImage_5);
-        if ($('#50').hasClass("selected")) {
-            confirm('Masz dodatkowe punkty,albo coś');
+
+        var k = j = 0;
+        while( j < 5 && k++ < 20 ){
+            randTd = rand(1,64); // losowanie liczby
+            if( $('table td').eq( randTd ).is(':empty') ){
+                $('table td').eq( randTd ).append( eval( 'targetImage_' + ( ++j ) ) );
+            }else{
+                console.log( 'Konflikt na polu ' + randTd );
+            }
         }
+
+
+        //
+        //
+        //$('table td').eq(rand(1, 64)).append(targetImage_1);
+        //$('table td').eq(rand(1, 64)).append(targetImage_2);
+        //$('table td').eq(rand(1, 64)).append(targetImage_3);
+        //$('table td').eq(rand(1, 64)).append(targetImage_4);
+        //$('table td').eq(rand(1, 64)).append(targetImage_5);
+
+
+
+        //if ($('#50').hasClass("selected")) {
+        //    confirm('Masz dodatkowe punkty,albo coś');
+            //while( var randTd = rand(1,64) && $('table td').eq( randTd ).hasClass(targetImage) ){ }
+        //}
     }
 }
+
